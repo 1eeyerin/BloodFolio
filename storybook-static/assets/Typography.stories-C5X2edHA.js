36e64682a64aeb7485e429b41dee3ea9510061f2
yerin <dev.yerin@gmail.com>
2024-08-22 17:19:46 +0900

    feat: init files

---
+++ storybook-static/assets/Typography.stories-C5X2edHA.js
@@ -0,0 +1,46 @@
+import{T as z}from"./Typography-BhVsfQOx.js";import"./styled-components.browser.esm-Os6HxUGj.js";import"./index-CDs2tPxN.js";import"./tslib.es6-CRos2fHm.js";const K={title:"Components/Typography",component:z,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{children:{control:"text",description:"텍스트를 지정합니다."},color:{control:"color",description:"텍스트의 색상을 지정합니다."},weight:{control:"select",description:"텍스트의 굵기를 지정합니다.",options:["regular","semibold","bold"]},as:{control:"select",description:"텍스트의 태그를 지정합니다.",options:["p","span","div","h1","h2","h3","h4","h5","h6"]},variant:{control:"select",description:"텍스트의 크기를 지정합니다.<br/>💎 (13px / 14px / 15px / 16px / 20px)",options:["typography1","typography2","typography3","typography4","typography5"]}}},r={args:{children:"기본 텍스트",variant:"typography4"}},a={args:{children:"Typography1 텍스트",variant:"typography1"}},o={args:{children:"Typography2 텍스트",variant:"typography2"}},e={args:{children:"Typography3 텍스트",variant:"typography3"}},p={args:{children:"Typography4 텍스트",variant:"typography4"}},s={args:{children:"Typography5 텍스트",variant:"typography5"}},t={args:{children:"볼드 텍스트",weight:"bold"}},n={args:{children:"세미볼드 텍스트",weight:"semibold"}},c={args:{children:"커스텀 컬러 텍스트",color:"var(--color-primary)"}};var y,i,g;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: '기본 텍스트',
+    variant: 'typography4'
+  }
+}`,...(g=(i=r.parameters)==null?void 0:i.docs)==null?void 0:g.source}}};var u,h,d;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: 'Typography1 텍스트',
+    variant: 'typography1'
+  }
+}`,...(d=(h=a.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};var l,m,D;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Typography2 텍스트',
+    variant: 'typography2'
+  }
+}`,...(D=(m=o.parameters)==null?void 0:m.docs)==null?void 0:D.source}}};var C,T,B;e.parameters={...e.parameters,docs:{...(C=e.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    children: 'Typography3 텍스트',
+    variant: 'typography3'
+  }
+}`,...(B=(T=e.parameters)==null?void 0:T.docs)==null?void 0:B.source}}};var v,A,S;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    children: 'Typography4 텍스트',
+    variant: 'typography4'
+  }
+}`,...(S=(A=p.parameters)==null?void 0:A.docs)==null?void 0:S.source}}};var b,x,E;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    children: 'Typography5 텍스트',
+    variant: 'typography5'
+  }
+}`,...(E=(x=s.parameters)==null?void 0:x.docs)==null?void 0:E.source}}};var w,F,_;t.parameters={...t.parameters,docs:{...(w=t.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    children: '볼드 텍스트',
+    weight: 'bold'
+  }
+}`,...(_=(F=t.parameters)==null?void 0:F.docs)==null?void 0:_.source}}};var f,P,O;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: '세미볼드 텍스트',
+    weight: 'semibold'
+  }
+}`,...(O=(P=n.parameters)==null?void 0:P.docs)==null?void 0:O.source}}};var j,k,q;c.parameters={...c.parameters,docs:{...(j=c.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: '커스텀 컬러 텍스트',
+    color: 'var(--color-primary)'
+  }
+}`,...(q=(k=c.parameters)==null?void 0:k.docs)==null?void 0:q.source}}};const L=["Primary","Typography1","Typography2","Typography3","Typography4","Typography5","Bold","Semibold","CustomColor"];export{t as Bold,c as CustomColor,r as Primary,n as Semibold,a as Typography1,o as Typography2,e as Typography3,p as Typography4,s as Typography5,L as __namedExportsOrder,K as default};
---
+++ storybook-static/assets/preview-BQr98Ni7.js
@@ -0,0 +1,55 @@
+import{h as r,j as o}from"./styled-components.browser.esm-Os6HxUGj.js";import{c as t}from"./constants-E40uE4fu.js";import"./index-CDs2tPxN.js";import"./tslib.es6-CRos2fHm.js";const a=r`
+${t};
+
+html, body {
+    font-size: 100%;
+    line-height: 1;
+	font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+}
+
+html {
+    color: var(--color-foreground);
+    background-color: var(--color-base-background);
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+}
+
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+    display: block;
+}
+
+ol, ul {
+    list-style: none;
+}
+
+blockquote, q {
+    quotes: none;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 0;
+}
+
+a {
+    color: inherit;
+    text-decoration: inherit;
+}
+
+img {
+    max-width: 100%;
+}
+`,c=[e=>o.jsxs(o.Fragment,{children:[o.jsx(a,{}),o.jsx(e,{})]})],d={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{c as decorators,d as default};
